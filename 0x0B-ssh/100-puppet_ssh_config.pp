04a71ad7c45d326adc91c879335a18e40295b3a3
Ayooluwa Gideon OLOYEDE <ayooluwaoloyede71@gmail.com>
2024-07-02 08:41:51 -0700

    Quick-fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # Create a ssh config file for client  
-file.line { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path => 'etc/ssh/ssh_config',
+#  path => 'etc/ssh/ssh_config',
+  path => '/root/alx-system_engineering-devops/0x0B-ssh/100-puppet_ssh_config.pp',
   line => 'PasswordAuthentication no'
 }
 
-file.line { 'Declare identify file':
+file_line { 'Declare identify file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
+PasswordAuthentication no
