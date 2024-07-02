ccc1bf0fa5588046fc7016d85df5e7adf0d315f0
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-07-02 01:17:37 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#Create a ssh config file for client  
+file.line {'Turn off passwd auth':
+  ensure => 'present',
+  path => 'etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
+
+file.line { 'Declare identify file':
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
