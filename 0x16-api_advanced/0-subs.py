28b3fb63430ded5370409e50327aa76c0a6c9be5
Ayooluwa Gideon OLOYEDE <ayooluwaoloyede71@gmail.com>
2024-08-13 11:48:26 -0700

    Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
+    """
+    Documentation for the requests modules
+    """
 import requests
 
 headers =  {"user-agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
+    """
+    documentatin for method
+    """
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
