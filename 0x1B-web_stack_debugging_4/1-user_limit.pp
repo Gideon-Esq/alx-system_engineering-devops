f071e748f9cef392c306ef5d10ead89e8133292c
root <ayooluwaoloyede71@gmail.com>
2024-08-22 20:18:49 +0000

    Hot-fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
-#bbdjjdkdkdk
+# bbdjjdkdkdk
 
-#bsjdjjdkdkdkd
+# bsjdjjdkdkdkd
 exec {'increase-hard-file-limit-for-holberton-user':
 	command => 'sed -i "/holberton hard/s/5/5000/" /etc/security/limits.conf',
 	path	=> 'usr/local/bin/:/bin/'
 }
 
-#fjfkkkdkkkdldl
+# fjfkkkdkkkdldl
 exec {'increase-soft-file-limit-for-holberton-user':
 	command => 'sed -i "/holberton soft/s/4/5000/" /etc/security/limits.conf',
 	path => '/usr/local/bin/:/bin/'
